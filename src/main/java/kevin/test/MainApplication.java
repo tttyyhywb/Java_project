01047253e75156f75468aa415b828ab955b37034
kwyang <kwyang@thoughtworks.com>
2018-05-28 10:35:04 +0800

    [Kevin] add checkstyle

--- src/main/java/kevin/test/MainApplication.java
+++ src/main/java/kevin/test/MainApplication.java
@@ -3,6 +3,7 @@ package kevin.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import java.util.List;
 
 @SpringBootApplication
 public class MainApplication extends SpringBootServletInitializer {
