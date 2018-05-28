28dc0254a7892c83f6bd7f414e2742ef07b99358
kwyang <kwyang@thoughtworks.com>
2018-05-28 10:28:20 +0800

    [Kevin] fix the gradle plugin versio for spring boot progrom

--- src/main/java/kevin/test/MainApplication.java
+++ src/main/java/kevin/test/MainApplication.java
@@ -2,7 +2,7 @@ package kevin.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class MainApplication extends SpringBootServletInitializer {
